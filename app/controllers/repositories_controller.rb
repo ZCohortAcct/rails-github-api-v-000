a0831299111060e1b1d39fae70758b426af88e6b
NickisKnowledge <lifechangingstuff@gmail.com>
2019-05-09 12:54:46 -0500

    passing test

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,13 @@
 class RepositoriesController < ApplicationController
   
   def index
+    resp = Faraday.get 'https://api.github.com/user/repos' do |req|
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = "token #{session[:token]}"
+    end
+
+    @repos = JSON.parse(resp.body)
+    # binding.pry
   end
 
 end
